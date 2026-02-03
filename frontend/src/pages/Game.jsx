23ec6d41e80e318e8aec3463f990735206d72206
DLucia15 <dluciaroyon@gmail.com>
2026-02-03 16:41:10 +0100

    CommitAmbExempleIRouter

---
+++ frontend/src/pages/Game.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Game() {
+  return (
+    <div>
+      <h1>Juego</h1>
+      <Link to="/">Volver al inicio</Link>
+    </div>
+  );
+}
+
+export default Game;
